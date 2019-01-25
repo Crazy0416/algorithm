5420dc1d74615893c0da7c3fe8b57dd900aa6625
KIM MIN HO <minmin0416@naver.com>
2019-01-26 00:17:25 +0900

    Refactoring: 14888 문제를 파일 -> 프로젝트로 변경.

--- backjun/14888yeonsanjakkiwuanaughgi/main.cpp
+++ backjun/14888yeonsanjakkiwuanaughgi/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <math.h>
 #define INF 987654321
+
+
 using namespace std;
+
 void solution(int sequence[], int operatorCnt[], long long prev, int idx, long long &MAX, long long &MIN) {
     int operRemainCnt = 0;
+
     for(int i = 0; i < 4; i++) {
         operRemainCnt += operatorCnt[i];
         if(operatorCnt[i] != 0 && i == 0) {
@@ -25,6 +29,7 @@ void solution(int sequence[], int operatorCnt[], long long prev, int idx, long l
             operatorCnt[i]--;
             long long dump = abs(prev);
             long long result = dump / (long long)sequence[idx+1];
+
             if(prev < 0) {
                 result *= -1;
             }
@@ -32,6 +37,7 @@ void solution(int sequence[], int operatorCnt[], long long prev, int idx, long l
             operatorCnt[i]++;
         }
     }
+
     // 재귀 종결 조건
     if(operRemainCnt == 0){
         if(MAX < prev)
@@ -41,16 +47,19 @@ void solution(int sequence[], int operatorCnt[], long long prev, int idx, long l
         return;
     }
 }
+
 int main() {
     int N;
     int *sequence = new int[N];
     int operatorCnt[4];
     long long MAX = -INF, MIN = INF;
+
     cin >> N;
     for(int i = 0; i < N; i++)
         cin >> sequence[i];
     for(int i = 0; i < 4; i++)
         cin >> operatorCnt[i];
+
     solution(sequence, operatorCnt, sequence[0], 0, MAX, MIN);
     cout << MAX << endl << MIN;
     return 0;
